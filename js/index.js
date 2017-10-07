6eb38df82d9a091548d65c1b7fac8aae84720a49
Nathan Yuchi <nathanyuchi@berkeley.edu>
2017-10-07 04:31:01 -0700

    SPACES AND PLUS SIGN

--- js/index.js
+++ js/index.js
@@ -16,21 +16,21 @@ function showListings() {
             var price = listing.message.split(/\r?\n/)[1].split(" ")[0];
             var desc = listing.message.split(/\r?\n/)[3];
 
-            var html = "<div class = \'container\'>" +
-                          "<div class = \'col-md-6\'>" +
-                            "<h2> " + address + " - " + price + "</h2>" +
-                            "<h3>" + desc + "</h3>" +
-                          "</div>" +
-                          "<div class = \'col-md-6\'>" +
-                            "<iframe" +
-                              "width=\"600\"" +
-                              "height=\"450\""
-                              "frameborder=\"0\" style=\"border:0\"" +
+            var html = "<div class = \'container\'> " +
+                          "<div class = \'col-md-6\'> " +
+                            "<h2> " + address + " - " + price + "</h2> " +
+                            "<h3>" + desc + "</h3> " +
+                          "</div> " +
+                          "<div class = \'col-md-6\'> " +
+                            "<iframe " +
+                              "width=\"600\" " +
+                              "height=\"450\" " +
+                              "frameborder=\"0\" style=\"border:0\" " +
                               "src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyCMC_RzdmMB3AH0MXEu8OcedoD3P9Ik9uY" +
-                                "&q=" + address + " allowfullscreen>" +
-                            "</iframe>" +
-                          "</div>" +
-                        "</div>";
+                                "&q=" + address + " allowfullscreen> " +
+                            "</iframe> " +
+                          "</div> " +
+                        "</div> ";
             }
         });
       }
