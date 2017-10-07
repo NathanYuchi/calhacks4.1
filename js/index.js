32e63c172f98a43d8474f81b57c9b946489c8bd3
Nathan Yuchi <nathanyuchi@berkeley.edu>
2017-10-07 04:15:13 -0700

    string split

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
 function login() {
   FB.login(function(response) {
     if (response.status == 'connected') {
-      showMeme();
+      showListings();
     }
   })
 }
-function showMeme() {
+function showListings() {
   FB.api(
     "/375220902820299/feed?limit=100",
     function (response) {
       if (response && !response.error) {
-        response.data.map(function(meme) {
-          id = meme.id;
-          var parent = document.getElementById('listings')
-          var p = document.createElement('p')
-          var node = document.createTextNode(meme.message)
-          p.appendChild(node)
-          parent.appendChild(p);
+        response.data.map(function(listing) {
+
+          var address = listing.message.split(/\r?\n/)[0].replace(/\b\w/g, l => l.toUpperCase());
+          var price = listing.message.split(/\r?\n/)[1].split(" ")[0];
+          var desc = listing.message.split(/\r?\n/)[3];
+
+          var html = "<div class = \'container\'>" +
+                        "<div class = \'col-md-6\'>" +
+                          "<h2> " + address + " - " + price + "</h2>" +
+                          "<h3>" + desc + "</h3>" +
+                        "</div>" +
+                        "<div class = \'col-md-6\'>" +
+                        "</div>" +
+                      "</div>";
 
         });
       }
