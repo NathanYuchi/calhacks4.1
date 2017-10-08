6fb932c9454a6630640678be98f5ef287dc03364
Nathan Yuchi <nathanyuchi@berkeley.edu>
2017-10-08 00:47:46 -0700

    loc logic

--- js/index.js
+++ js/index.js
@@ -42,30 +42,31 @@ function showListings() {
 
             var apiAddress = address.split(' ').join('+');
             var lattitude = getLattitude(apiAddress);
-
-            var html = "<div class = \'row\'> " +
-                          "<div class = \'col-md-6\'> " +
-                            "<div class = \"col-sm-9\">" +
-                              "<h3> " + address + " - " + price + "</h3> " +
-                              "<h4>" + desc + "</h4> " +
-                            "</div>" +
-                            "<div class = \"col-sm-3\">" +
-                              "<a href=\"" + link +"\" class=\"btn btn-primary\" role=\"button\" target=\"_blank\">Link to Posting</a>" +
-                            "</div>" +
-                          "</div> " +
-                          "<div class = \'col-md-6\'> " +
-                            "<iframe " +
-                              "width=\"400\" " +
-                              "height=\"400\" " +
-                              "frameborder=\"0\" style=\"border:0\" " +
-                              "src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyCx8NsG_fJqJNWEtcnt5Wgy14x1B0ds8Ic" +
-                                "&q=" + apiAddress + "\" allowfullscreen> " +
-                            "</iframe> " +
-                          "</div> " +
-                          "<hr class=\"section-heading-spacer\"> " +
-                          "<div class=\"clearfix\"> </div> " +
-                        "</div> <br> <br>";
-              $("#listings").append(html);
+            if (currLoc === "1" && (lattitude <= middleLat) || (currLoc === "2" && (lattitude >= middleLat))) {
+              var html = "<div class = \'row\'> " +
+                            "<div class = \'col-md-6\'> " +
+                              "<div class = \"col-sm-9\">" +
+                                "<h3> " + address + " - " + price + "</h3> " +
+                                "<h4>" + desc + "</h4> " +
+                              "</div>" +
+                              "<div class = \"col-sm-3\">" +
+                                "<a href=\"" + link +"\" class=\"btn btn-primary\" role=\"button\" target=\"_blank\">Link to Posting</a>" +
+                              "</div>" +
+                            "</div> " +
+                            "<div class = \'col-md-6\'> " +
+                              "<iframe " +
+                                "width=\"400\" " +
+                                "height=\"400\" " +
+                                "frameborder=\"0\" style=\"border:0\" " +
+                                "src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyCx8NsG_fJqJNWEtcnt5Wgy14x1B0ds8Ic" +
+                                  "&q=" + apiAddress + "\" allowfullscreen> " +
+                              "</iframe> " +
+                            "</div> " +
+                            "<hr class=\"section-heading-spacer\"> " +
+                            "<div class=\"clearfix\"> </div> " +
+                          "</div> <br> <br>";
+                $("#listings").append(html);
+              }
             }
         });
       }
@@ -85,7 +86,7 @@ function getLattitude(address) {
         async: false,
         headers: { "Accept": "application/json;odata=verbose"}, // return data format
         success: function (data) {
-         console.log(data);
+         lattitude = data.results[0].geometry.location.lat;
         },
         error: function (error) {
         }
